7d2d6989aaaad54a65b8783e898dad302bff3686
SaravananSundar <saravananmech020797@gmail.com>
2024-05-08 05:27:38 +0530

    remove

--- src/About.js
+++ src/About.js
@@ -4,9 +4,7 @@ const About = () =>{
   return (
     <main className='about'>
     <h2>About</h2>
-    <p style={{ marginTop: "1rem"}}> Hi, I am
-    Balachandar. Together lets make education
-    free for everyone in the world</p>
+    <p style={{ marginTop: "1rem"}}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio alias suscipit tenetur? Dolor, sit in rerum, cumque quam nesciunt omnis placeat exercitationem facere, expedita molestiae? Ex hic assumenda tenetur beatae?</p>
     </main>
   )
 }
